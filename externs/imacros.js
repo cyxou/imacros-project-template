c79a81445b066ca214d4a2c463d00858e65d7fbe
cyxou <acyxou@gmail.com>
2016-07-20 17:41:17 +0300

    Build scripts optimizations

---
+++ externs/imacros.js
@@ -0,0 +1,35 @@
+const global = {};
+global.BOT_CONFIG = {};
+global.STAGE = '';
+global.AUTH_TOKEN = '';
+
+const Components = {};
+Components.utils = {};
+
+const imns = {};
+imns.Cu = {};
+imns.Cc = {};
+imns.Ci = {};
+imns.FIO = {};
+imns.FIO.makeDirectory = function(path) {}
+imns.FIO.appendTextFile = function(file, data) {}
+
+/**
+ * @param {string} msg
+ */
+function iimDisplay(msg) {}
+
+/**
+ * @param {string} iimCode
+ */
+function iimPlayCode(iimCode) {}
+function iimGetLastError() {}
+function iimGetLastExtract() {}
+
+/**
+ * require
+ * @param {string} name A path to a module required
+ */
+function require(name) {}
+const module = {};
+const exports = {};
